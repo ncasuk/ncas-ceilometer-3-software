b5ca309e09d36d8735af59050b62d6e91e5e4d0a
Dan Walker <daniel.walker@ncas.ac.uk>
2018-09-11 16:24:26 +0100

    Change to tabstop=4; move test code into class method

--- CRC_CS135.py
+++ CRC_CS135.py
@@ -1,40 +1,49 @@
-#!/usr/bin/python
-# coding=utf8
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8 expandtab ts=4:
+# -*- coding: utf-8 -*-
 
 class CRC_CS135:
-   def __init__(self):
-      self.tab=256*[[]]
-      for i in range(256):
-         crc=0
-         c = i << 8
+    """
+    Class implementing the CRC for the Campbell Scientific CS135 ceilometer
+    """
 
-         for j in range(8):
-            if (crc ^ c) & 0x8000:
-               crc = ( crc << 1) ^ 0x1021
-            else:
-                  crc = crc << 1
+    initval=0xFFFF
+    xorval=0xFFFF
 
-            c = c << 1
+    def __init__(self):
+        self.tab=256*[[]]
+        for i in range(256):
+            crc=0
+            c = i << 8
 
-            crc = crc & 0xffff
+            for j in range(8):
+                if (crc ^ c) & 0x8000:
+                    crc = ( crc << 1) ^ 0x1021
+                else:
+                        crc = crc << 1
 
-         self.tab[i]=crc
-   
-   def update_crc(self, crc, c):
-      short_c=0x00ff & (c % 256)
+                c = c << 1
 
-      tmp = ((crc >> 8) ^ short_c) & 0xffff
-      crc = (((crc << 8) ^ self.tab[tmp])) & 0xffff
+                crc = crc & 0xffff
 
-      return crc
+            self.tab[i]=crc
+    
+    def update_crc(self, crc, c):
+        short_c=0x00ff & (c % 256)
 
-test=CRC_CCITT()
+        tmp = ((crc >> 8) ^ short_c) & 0xffff
+        crc = (((crc << 8) ^ self.tab[tmp])) & 0xffff
 
-teststr=b"Hello World"
+        return crc
 
-crcval=0xFFFF
+    def crc_message(self, message):
+        """
+        Takes Python3 bytes buffer and CRCs it using the algorithm
+        used by the CS135 ceilometer
+        """
 
-for c in teststr:
-   crcval=test.update_crc(crcval, c)
+        crcval = self.initval
+        for c in message:
+            crcval=self.update_crc(crcval, c)
 
-print(crcval ^ 0xFFFF)
+        return crcval ^ self.xorval
